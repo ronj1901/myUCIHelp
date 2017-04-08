2fcc4ec134dcfadbc461156eb82123f7f1166b1f
sam <thapashambhu16@yahoo.com>
2017-04-08 15:48:18 -0700

    added profile activity

--- app/src/main/java/nextbest/myuci/MainActivity.java
+++ app/src/main/java/nextbest/myuci/MainActivity.java
@@ -1,13 +1,21 @@
 package nextbest.myuci;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabaseReference;
+
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
